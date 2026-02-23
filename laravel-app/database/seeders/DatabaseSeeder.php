be920bd4ba0628bce72a970159bc677506b49762
Wilsent Philip <wphilip465@gmail.com>
2026-02-23 19:34:05 +0700

    prod: create 1 admin and 1 regular user

--- laravel-app/database/seeders/DatabaseSeeder.php
+++ laravel-app/database/seeders/DatabaseSeeder.php
@@ -19,37 +19,29 @@ class DatabaseSeeder extends Seeder
             // ProductSeeder::class,
             DefectTypeSeeder::class,
             // MockSeeder::class,
-
-
         ]);
 
-        User::factory(3)->updateOrCreate()->each(
-            function ($user, $index) {
-                if ($index == 0) {
-                    $user->update([
-                        'name' => 'admin ' . $index,
-                        'role' => 'admin',
-                        'email' => 'admin@example.com',
-                        'password' => bcrypt('123456'),
-                    ]);
-                }
-                if ($index == 1) {
-                    $user->update([
-                        'name' => 'user ' . $index,
-                        'role' => 'user',
-                        'email' => 'user@example.com',
-                        'password' => bcrypt('123456'),
-                    ]);
-                }
-                if ($index == 2) {
-                    $user->update([
-                        'name' => 'admin ' . $index,
-                        'role' => 'admin',
-                        'email' => 'admin2@example.com',
-                        'password' => bcrypt('123456'),
-                    ]);
-                }
-            }
+        # Create admin user
+        User::updateOrCreate(
+            [
+                'email' => 'admin@example.com'
+            ],
+            [
+                'name' => 'Admin',
+                'role' => 'admin',
+                'password' => bcrypt('123456'),
+            ]
         );
+
+        # Create regular user
+        User::updateOrCreate(
+            [ 'email' => 'user@example.com' ],
+            [
+                'name' => 'User',
+                'role' => 'user',
+                'password' => bcrypt('123456'),
+            ]
+        );
+
     }
 }
